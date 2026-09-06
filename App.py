4660a51dc83b47d2dc942c27a4f1b3593da3a182
ousmanesourabie972-arch <ousmanesourabie972@gmail.com>
2026-09-06 03:12:55 +0000

    Update App.py

--- App.py
+++ App.py
@@ -1,2 +1,12 @@
-print("Football Predictor")
-print("Robot de prédiction des matchs")
+print("⚽ FOOTBALL PREDICTOR")
+
+equipe1 = input("Nom de la première équipe : ")
+equipe2 = input("Nom de la deuxième équipe : ")
+
+print()
+print("Analyse du match...")
+print("Match :", equipe1, "vs", equipe2)
+print()
+print("🎯 Prédiction :")
+print("1X - Victoire de", equipe1, "ou Match nul")
+print("Score possible : 1-1")
